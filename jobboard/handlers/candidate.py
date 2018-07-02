46ecf31985cbeb28b3f1b982a7b00d5d4e44eb8b
Dmitriy <d.romanov.@ongrid.pro>
2018-07-02 11:44:11 +0300

    VJB-41 Change contract interfaces

---
+++ jobboard/handlers/candidate.py
@@ -0,0 +1,37 @@
+import json
+
+from django.conf import settings
+from solc import compile_files
+from solc.utils.string import force_bytes
+from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
+
+
+class CandidateHandler(object):
+    def __init__(self, contract_address, account=None):
+        self.web3 = Web3(HTTPProvider(settings.NODE_URL))
+        self.web3.middleware_stack.inject(geth_poa_middleware, layer=0)
+        self.account = account or settings.WEB_ETH_COINBASE
+        self.contract_address = contract_address
+        self.__password = settings.COINBASE_PASSWORD_SECRET
+        try:
+            with open(settings.ABI_PATH + 'Candidate.abi.json', 'r') as ad:
+                self.abi = json.load(ad)
+        except FileNotFoundError:
+            path = 'dapp/contracts/Candidate.sol'
+            compiled = compile_files([path, ],
+                                     output_values=("abi", "ast", "bin", "bin-runtime",))
+            with open(settings.ABI_PATH + 'Candidate.abi.json', 'w+') as ad:
+                ad.write(json.dumps(compiled[path + ':Candidate']['abi']))
+                self.abi = compiled[path + ':Candidate']['abi']
+        self.contract = self.web3.eth.contract(abi=self.abi, address=self.contract_address)
+
+    def unlockAccount(self):
+        self.web3.personal.unlockAccount(self.account, self.__password)
+
+    def get_id(self):
+        return self.contract.call().id()
+
+    def subscribe(self, vac_uuid):
+        self.unlockAccount()
+        return self.contract.transact({'from': self.account}).subscribe(vac_uuid)
--- jobboard/handlers/coin.py
+++ jobboard/handlers/coin.py
@@ -1,5 +1,7 @@
 from web3 import Web3, HTTPProvider
 import json
+
+from web3.middleware import geth_poa_middleware
 from web3.utils.validation import validate_address
 from django.conf import settings
 
@@ -7,8 +9,9 @@ from django.conf import settings
 class CoinHandler(object):
     def __init__(self, contract_address=None, account=None):
         self.web3 = Web3(HTTPProvider('http://localhost:8545'))
+        self.web3.middleware_stack.inject(geth_poa_middleware, layer=0)
         self.contract_address = contract_address or settings.VERA_COIN_CONTRACT_ADDRESS
-        self.account = account
+        self.account = account or settings.WEB_ETH_COINBASE
         with open('jobboard/handlers/coin_abi.json', 'r') as ad:
             self.abi = json.load(ad)
         self.contract = self.web3.eth.contract(abi=self.abi, address=self.contract_address)
